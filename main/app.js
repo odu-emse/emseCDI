00a3b3c4ddb4652851e5e28ae34833a4c61cc235
Daniel Papp <dpapp001@odu.edu>
2021-10-14 13:48:10 -0400

    ⏪️ (main thread): Added dir listing script
    We can use this script to parse local fs and get module directories

--- main/app.js
+++ main/app.js
@@ -1,19 +1,25 @@
-"use strict";
+'use strict'
+var __importDefault =
+    (this && this.__importDefault) ||
+    function (mod) {
+        return mod && mod.__esModule ? mod : { default: mod }
+    }
+Object.defineProperty(exports, '__esModule', { value: true })
+exports.dir = void 0
 // @ts-ignore
-// import fs from 'fs'
+const fs_1 = __importDefault(require('fs'))
 //
-// export const dir = () => {
-//
-// fs.readdir(`${__dirname}/../assets/modules`, (err, files) => {
-//     console.log('dir')
-//     if (err)
-//         console.log(err);
-//     else {
-//         console.log("\nCurrent directory filenames:");
-//         files.forEach(file => {
-//             console.log(file)
-//         })
-//     }
-// })
-// }
-console.log('here');
+const dir = () => {
+    fs_1.default.readdir(`${__dirname}/../assets/modules`, (err, files) => {
+        console.log('dir')
+        if (err) console.log(err)
+        else {
+            console.log('\nCurrent directory filenames:')
+            files.forEach((file) => {
+                console.log(file)
+            })
+        }
+    })
+}
+exports.dir = dir
+console.log('here')
