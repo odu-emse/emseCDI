278f1aa70f28ddddce911da1b1d4edaa27431452
Daniel Papp <dpapp001@odu.edu>
2021-10-21 10:19:32 -0400

    ✨ (Home): Introduced AODP-25 feature
    Now the Home component reads the index.md file at the root of the modules folder to give students an insight into the course.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,47 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from './Layout'
+import Markdown from 'markdown-to-jsx'
+import { getData } from '../helper/fetch'
 
 const Home = () => {
+    const [courseContent, setCourseContent] = useState('# Welcome to ENMA 600')
+
+    useEffect(() => {
+        let mounted = true
+        getData('../assets/modules/index.md', 'md').then((res) => {
+            if (mounted) {
+                setCourseContent(res)
+            }
+        }).catch(e => console.error(e))
+        return function cleanup(){
+            mounted = false
+        }
+    }, [])
     return (
         <Layout>
-          <h1 className='text-3xl'>This is the Home.tsx component</h1>
+            <Markdown
+                options={{
+                    overrides: {
+                        h1: {
+                            props: {
+                                className: 'text-2xl font-bold text-gray-800',
+                            },
+                        },
+                        h2: {
+                            props: {
+                                className: 'text-xl pb-2 text-gray-700',
+                            },
+                        },
+                        h3: {
+                            props: {
+                                className: 'text-lg pb-1 text-gray-500',
+                            },
+                        },
+                    },
+                }}
+            >
+                {courseContent}
+            </Markdown>
         </Layout>
     )
 }
