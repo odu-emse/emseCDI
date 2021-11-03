50ce50931213f856cc91bbda259a7daf88b5c2f4
Daniel Papp <dpapp001@odu.edu>
2021-11-03 14:37:42 -0400

    💄 (program): Added Resources and Exercises components
    Now when we swtich the tab on the Module page the Exercises and Resources tabs fetch the corresponding components and pass down data to render

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -6,6 +6,8 @@ import ReactPlayer from 'react-player'
 import ModuleContent from './ModuleContent'
 import FileSaver from 'file-saver'
 import Button from './Button'
+import Resources from './Resources'
+import Exercises from './Exercises'
 
 const Module: React.FC = (props) => {
     const [page, setPage] = useState('1')
@@ -17,6 +19,7 @@ const Module: React.FC = (props) => {
     const [notes, setNotes] = useState('')
     const [sideBySide, setSideBySide] = useState(false)
     const [active, setActive] = useState('Overview')
+    const [dir, setDir] = useState([])
 
     const {
         //@ts-ignore
@@ -28,6 +31,13 @@ const Module: React.FC = (props) => {
     useEffect(() => {
         setPage(params.id)
         setVid(params.videoID)
+
+        // @ts-ignore
+        window.api.send('toMain', '_')
+        // @ts-ignore
+        window.api.receive('fromMain', (data: any) => {
+            setDir(data)
+        })
 
         let time
         let data
@@ -54,8 +64,6 @@ const Module: React.FC = (props) => {
         //         vid.currentTime = 0
         //     }
         // }
-
-        console.log(params)
 
         const seed = `../../assets/modules/${page}/index.md`
         const src = `../../assets/modules/${page}/${vid}.mp4`
@@ -110,7 +118,7 @@ const Module: React.FC = (props) => {
                 Switch views
             </Button>
             <div
-                className={`flex ${
+                className={`flex mt-3 ${
                     sideBySide
                         ? 'flex-row justify-between items-start'
                         : 'flex-col items-center'
@@ -128,150 +136,189 @@ const Module: React.FC = (props) => {
                         controls={true}
                     />
                 </div>
-              <section className="flex flex-row w-full my-4" style={{boxShadow: 'inset 0 -2px rgba(0,0,0,0.1)'}}>
-                  <Button
-                    size="small"
-                    name="Search"
-                    variant={
-                      active === 'Search' ? 'active' : 'default'
-                    }
-                    onClick={(e:object) => setActive(e.target.name)}
-                    className="pr-4 pl-0"
-                  >
-                    Search transcript
-                  </Button>
-                  <Button
-                    size="small"
-                    name="Overview"
-                    variant={
-                      active === 'Overview' ? 'active' : 'default'
-                    }
-                    onClick={(e:object) => setActive(e.target.name)}
-                    className="px-4"
-                  >
-                    Overview
-                  </Button>
-                  <Button
-                    size="small"
-                    name="Notes"
-                    variant={
-                      active === 'Notes' ? 'active' : 'default'
-                    }
-                    onClick={(e:object) => setActive(e.target.name)}
-                    className="px-4"
-                  >
-                    Notes
-                  </Button>
-                  <Button
-                    size="small"
-                    name="Resources"
-                    variant={
-                      active === 'Resources' ? 'active' : 'default'
-                    }
-                    onClick={(e:object) => setActive(e.target.name)}
-                    className="px-4"
-                  >
-                    Resources
-                  </Button>
-                  <Button
-                    size="small"
-                    name="Exercises"
-                    variant={
-                      active === 'Exercises' ? 'active' : 'default'
-                    }
-                    onClick={(e:object) => setActive(e.target.name)}
-                    className="px-4"
-                  >
-                    Exercises
-                  </Button>
-              </section>
-                {/*Search section*/}
-                <section className={`w-full ${active === 'Search' ? 'visible' : 'hidden'}`}>
-                  <input className={`shadow-inner w-full border-2 border-gray-100 focus:ring-2 focus:ring-blue-400 outline-none py-2 px-4 my-3 transition-all`} type='text' placeholder={`Search through lesson's transcript`}/>
-                </section>
-                {/*Overview section*/}
-                <section className={`${active === 'Overview' ? 'visible' : 'hidden'}`}>
-                  <ModuleContent content={post} />
-                </section>
-
-                {/*Notes section*/}
-                <section
-                    className={`my-5 bg-gray-100 shadow-md py-2 px-2 border border-gray-200 ${
-                        sideBySide ? 'w-full ml-2' : 'w-full'
-                    } ${active === 'Notes' ? 'visible' : 'hidden'}`}
+                <div
+                    className={`w-full ${
+                        sideBySide ? 'flex flex-col' : 'block'
+                    }`}
                 >
-                    <div className="flex flex-row">
+                    <section
+                        className="flex flex-row w-full my-4"
+                        style={{ boxShadow: 'inset 0 -2px rgba(0,0,0,0.1)' }}
+                    >
                         <Button
                             size="small"
-                            onClick={() => setPreview('edit')}
-                            variant={
-                                preview === 'edit' ? 'tabActive' : 'tabInactive'
-                            }
-                            className="px-4"
+                            name="Search"
+                            variant={active === 'Search' ? 'active' : 'default'}
+                            onClick={(e: object) => setActive(e.target.name)}
+                            className="pr-4 pl-0"
                         >
-                            Edit
+                            Search transcript
                         </Button>
                         <Button
                             size="small"
-                            onClick={() => setPreview('live')}
+                            name="Overview"
                             variant={
-                                preview === 'live' ? 'tabActive' : 'tabInactive'
+                                active === 'Overview' ? 'active' : 'default'
                             }
+                            onClick={(e: object) => setActive(e.target.name)}
                             className="px-4"
                         >
-                            Live edit
+                            Overview
                         </Button>
                         <Button
                             size="small"
-                            onClick={() => setPreview('preview')}
-                            variant={
-                                preview === 'preview'
-                                    ? 'tabActive'
-                                    : 'tabInactive'
-                            }
+                            name="Notes"
+                            variant={active === 'Notes' ? 'active' : 'default'}
+                            onClick={(e: object) => setActive(e.target.name)}
                             className="px-4"
                         >
-                            Preview
+                            Notes
                         </Button>
-                    </div>
-
-                    <section className="mb-2 border-0">
-                        <MDEditor
-                            value={notes}
-                            onChange={setNotes}
-                            hideToolbar={true}
-                            preview={preview}
+                        <Button
+                            size="small"
+                            name="Resources"
+                            variant={
+                                active === 'Resources' ? 'active' : 'default'
+                            }
+                            onClick={(e: object) => setActive(e.target.name)}
+                            className="px-4"
+                        >
+                            Resources
+                        </Button>
+                        <Button
+                            size="small"
+                            name="Exercises"
+                            variant={
+                                active === 'Exercises' ? 'active' : 'default'
+                            }
+                            onClick={(e: object) => setActive(e.target.name)}
+                            className="px-4"
+                        >
+                            Exercises
+                        </Button>
+                    </section>
+                    {/*Search section*/}
+                    <section
+                        className={`w-full ${
+                            active === 'Search' ? 'visible' : 'hidden'
+                        }`}
+                    >
+                        <input
+                            className={`shadow-inner w-full border-2 border-gray-100 focus:ring-2 focus:ring-blue-400 outline-none py-2 px-4 my-3 transition-all`}
+                            type="text"
+                            placeholder={`Search through lesson's transcript`}
                         />
                     </section>
-                    <div className="flex justify-end">
-                        <Button
-                            size="small"
-                            variant="secondary"
-                            className="w-1/5 ml-auto block rounded-lg mb-2"
-                        >
-                            Load notes
-                        </Button>
-                        <Button
-                            size="small"
-                            onClick={() => {
-                                let data = new Blob([notes], {
-                                    type: 'text/plain;charset=utf-8',
-                                })
-                                FileSaver.saveAs(data, 'notes.md')
-                            }}
-                            variant="success"
-                            className="w-1/5 ml-2 block rounded-lg mb-2"
-                        >
-                            Save notes
-                        </Button>
-                    </div>
-                    <i className="text-xs text-gray-500 text-right block w-full">
-                        Please note that in order to save your notes you need to
-                        have write privileges on your computer.
-                    </i>
-                </section>
-            </div>
 
+                    {/*Overview section*/}
+                    <section
+                        className={`${
+                            active === 'Overview' ? 'visible' : 'hidden'
+                        }`}
+                    >
+                        <ModuleContent content={post} />
+                    </section>
+
+                    {/*Notes section*/}
+                    <section
+                        className={`my-5 bg-gray-100 shadow-md py-2 px-2 border border-gray-200 ${
+                            sideBySide ? 'w-full ml-2' : 'w-full'
+                        } ${active === 'Notes' ? 'visible' : 'hidden'}`}
+                    >
+                        <div className="flex flex-row">
+                            <Button
+                                size="small"
+                                onClick={() => setPreview('edit')}
+                                variant={
+                                    preview === 'edit'
+                                        ? 'tabActive'
+                                        : 'tabInactive'
+                                }
+                                className="px-4"
+                            >
+                                Edit
+                            </Button>
+                            <Button
+                                size="small"
+                                onClick={() => setPreview('live')}
+                                variant={
+                                    preview === 'live'
+                                        ? 'tabActive'
+                                        : 'tabInactive'
+                                }
+                                className="px-4"
+                            >
+                                Live edit
+                            </Button>
+                            <Button
+                                size="small"
+                                onClick={() => setPreview('preview')}
+                                variant={
+                                    preview === 'preview'
+                                        ? 'tabActive'
+                                        : 'tabInactive'
+                                }
+                                className="px-4"
+                            >
+                                Preview
+                            </Button>
+                        </div>
+
+                        <section className="mb-2 border-0">
+                            <MDEditor
+                                value={notes}
+                                onChange={setNotes}
+                                hideToolbar={true}
+                                preview={preview}
+                            />
+                        </section>
+                        <div className="flex justify-end">
+                            <Button
+                                size="small"
+                                variant="secondary"
+                                className="w-1/5 ml-auto block rounded-lg mb-2"
+                            >
+                                Load notes
+                            </Button>
+                            <Button
+                                size="small"
+                                onClick={() => {
+                                    let data = new Blob([notes], {
+                                        type: 'text/plain;charset=utf-8',
+                                    })
+                                    FileSaver.saveAs(data, 'notes.md')
+                                }}
+                                variant="success"
+                                className="w-1/5 ml-2 block rounded-lg mb-2"
+                            >
+                                Save notes
+                            </Button>
+                        </div>
+                        <i className="text-xs text-gray-500 text-right block w-full">
+                            Please note that in order to save your notes you
+                            need to have write privileges on your computer.
+                        </i>
+                    </section>
+
+                    {/*Resources section*/}
+                    <section
+                        className={`${
+                            active === 'Resources' ? 'visible' : 'hidden'
+                        }`}
+                    >
+                        <Resources data={dir} module={page} />
+                    </section>
+
+                    {/*Exercises section*/}
+                    <section
+                        className={`${
+                            active === 'Exercises' ? 'visible' : 'hidden'
+                        }`}
+                    >
+                        <Exercises data={dir} module={page} />
+                    </section>
+                </div>
+            </div>
         </Layout>
     )
 }
