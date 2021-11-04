b8a375de4cf00bc8f1976b2f7635b74985c94313
Daniel Papp <dpapp001@odu.edu>
2021-11-04 12:02:16 -0400

    🏷️ (button): Updated onClick parameter type

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 interface Props {
     children?: React.ReactNode
-    onClick?: () => void
+    onClick?: (e?: object) => void
     variant: string
     className: string
     size: string
